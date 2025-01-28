e3db873b43e0b47af075a5d177e175cdb1af2a8e
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-01-28 16:37:12 +0530

    Update Fruits.jsx

--- webbro/src/Fruits.jsx
+++ webbro/src/Fruits.jsx
@@ -1,17 +1,21 @@
 function Fruits(){
 
-  const fruits= [ { name:"apple", cals: 95} ,
-     { name:"banana" , cals: 105} ,
-      { name:"cherry", cals: 50} ,
-     { name: "date" , cals:  66} ,
-      { name:"elderberry" , cals:  32} ,
+  const fruits= [ { id: 1, name:"apple", cals: 95} ,
+     { id:2 , name:"banana" , cals: 105} ,
+      {  id:3 , name:"cherry" , cals: 50} ,
+     { id:4,  name: "date" , cals:  66} ,
+      {  id:5, name:"elderberry" , cals:  32} ,
      ];
-  fruits.sort();
-  const listItems =fruits.map(fruit => <li key ={fruit.name}> 
-                     {fruit.name}: &nbsp;
-                     {fruit.cals}
+  //fruits.sort ((a,b)=> a.cals - b.cals);// Numeric order 
+
+  // IN order to sort using cals we use 
+
+  const lowCalFruits = fruits.filter(fruit => fruit.cals <100); // we use filter to filter out the low cals frits 
+  const listItems =lowCalFruits.map(lowCalFruits => <li key ={lowCalFruits.id}> 
+                     {lowCalFruits.name}: &nbsp;
+                   <b>  {lowCalFruits.cals}</b>
                         
-                         </li>);
+                         </li>);  // we use .map to itreate over each item in the array
 
   return(
 <ol>{listItems}</ol>
