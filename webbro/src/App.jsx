bf2bd7e98b2b34da27c12d63f1d8c5683e32cd43
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-01-29 13:41:45 +0530

    n
    n

--- webbro/src/App.jsx
+++ webbro/src/App.jsx
@@ -1,9 +1,17 @@
-import Geet from "./Geet.jsx"
+
 import Fruits from "./Fruits.jsx";
 
 function App() {
+
+
+  const fruits=   [ { id: 1, name:"apple", cals: 95} ,
+                     { id:2 , name:"banana" , cals: 105} ,
+                      {  id:3 , name:"cherry" , cals: 50} ,
+                       { id:4,  name: "date" , cals:  66} ,
+                      {  id:5, name:"elderberry" , cals:  32} ,
+    ];
   return (
-    <Fruits/>
+    <  Fruits  items={fruits}  cato="frus"  />
   );
 }
 export default App;
--- webbro/src/Fruits.jsx
+++ webbro/src/Fruits.jsx
@@ -1,25 +1,21 @@
-function Fruits(){
+function Fruits(props) {
 
-  const fruits= [ { id: 1, name:"apple", cals: 95} ,
-     { id:2 , name:"banana" , cals: 105} ,
-      {  id:3 , name:"cherry" , cals: 50} ,
-     { id:4,  name: "date" , cals:  66} ,
-      {  id:5, name:"elderberry" , cals:  32} ,
-     ];
-  //fruits.sort ((a,b)=> a.cals - b.cals);// Numeric order 
 
-  // IN order to sort using cals we use 
+   const itemList = props.itmes;
 
-  const lowCalFruits = fruits.filter(fruit => fruit.cals <100); // we use filter to filter out the low cals frits 
-  const listItems =lowCalFruits.map(lowCalFruits => <li key ={lowCalFruits.id}> 
-                     {lowCalFruits.name}: &nbsp;
-                   <b>  {lowCalFruits.cals}</b>
-                        
-                         </li>);  // we use .map to itreate over each item in the array
 
-  return(
-<ol>{listItems}</ol>
-    
-  )
+  const listItems = itemList.map((item) => (
+    <li key={lowCalFruits.id}>
+      {lowCalFruits.name}: &nbsp;
+      <b> {lowCalFruits.cals}</b>
+    </li>
+  ));
+  return <ol>{listItems}</ol>;
 }
 export default Fruits;
+
+//fruits.sort ((a,b)=> a.cals - b.cals);// Numeric order
+// we use .map to itreate over each item in the array
+
+// IN order to sort using cals we use
+//const lowCalFruits = fruits.filter(fruit => fruit.cals <100); // we use filter to filter out the low cals frits
